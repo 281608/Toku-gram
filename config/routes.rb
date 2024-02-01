8ea5b4ba6eec685630d22e6016e674af393cafb2
EC2 Default User <ec2-user@ip-172-31-28-63.ap-southeast-2.compute.internal>
2024-02-01 13:59:27 +0000

    Toku-gram(ルーティングまでしている）

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/home/about" => "homes#about", as: "about"
+  resources :users, only: [:index, :show, :edit]
+  resources :posts, only: [:new, :index, :show, :edit]
+  root to: "homes#top"
+  devise_for :users
+ #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
