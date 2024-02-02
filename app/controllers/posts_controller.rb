d82a5dfd9e08c087803c9189e32ff54c3c806dfc
EC2 Default User <ec2-user@ip-172-31-28-63.ap-southeast-2.compute.internal>
2024-02-02 11:50:56 +0000

    投稿一覧まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,24 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+   if @post.save
+       flash[:notice] = "You have created book successfully."
+    redirect_to post_path(@post.id)
+   else
+     @posts = Post.all
+     @user = current_user
+     render :index
+   end
   end
 
   def index
+    @posts = Post.all
+    @user = current_user
   end
 
   def show
@@ -10,4 +26,18 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to posts_path
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @users = User.all
   end
 
   def show
   end
 
   def edit
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to posts_path
+    end
   end
 end
